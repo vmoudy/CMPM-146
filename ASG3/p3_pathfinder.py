54e6592368ab065700cbc2733ddca954fa331f3c
vmoudy <vpmoudy@gmail.com>
2015-04-21 11:46:53 -0700

    Update p3_pathfinder.py
    trying to implement A*

--- ASG3/p3_pathfinder.py
+++ ASG3/p3_pathfinder.py
@@ -3,8 +3,14 @@ import random
 import Queue
 
 def find_path(source_point, destination_point, mesh):
-	queue = Queue.Queue()
+	#BFS queue
+	#queue = Queue.Queue()
+
+	#A* queue
+	queue = Queue.PriorityQueue()
+
 	prev = {}
+	cost_so_far = {}
 	prev_box = {}
 	visited = []
 	detail_points = {}
@@ -18,13 +24,20 @@ def find_path(source_point, destination_point, mesh):
 	if source_box == dest_box:
 		return ([(source_point, destination_point)], [(source_box)])
 
-	queue.put(source_box)
+	#BFS queue
+	#queue.put(source_box)
+
+	#A* queue
+	queue.put(source_box, 0)
+	cost_so_far[source_box] = 0
+
 	prev[source_box] = None
 	prev_box[source_box] = source_box
 	detail_points[source_box] = source_point
 	detail_points[dest_box] = destination_point
 
-	while not queue.empty():
+	#BFS with early exit
+	"""while not queue.empty():
 		current_box = queue.get()		
 
 		if current_box == dest_box:
@@ -41,14 +54,41 @@ def find_path(source_point, destination_point, mesh):
 		except:
 			print "Not a valid starting point."
 			return ([], [])
+	"""
 
+
+	#A*
+	while not queue.empty():
+		current_box = queue.get()
+
+		if current_box == dest_box:
+			break
+
+		try:
+			for next in mesh['adj'][current_box]:
+				src = detail_points[current_box]
+				dest = find_detail_points(src, next)
+				new_cost = cost_so_far[current_box] + cost_path(src, dest)
+				if next not in cost_so_far or new_cost < cost_so_far[next]:
+					detail_points[next] = find_detail_points(detail_points[current_box], next)
+					cost_so_far[next] = new_cost
+					priority = heuristic(detail_points[next], destination_point)
+					queue.put(next, priority)
+					prev[next] = current_box
+					prev_box[next] = current_box
+					visited.append(next)
+		except:
+			print "Not a valid starting point."
+			return([], [])
+
+
+	#find path working backwards to source
 	if current_box == dest_box:
 		while current_box:
 			path.append((detail_points[prev_box[current_box]], detail_points[current_box]))
 			if prev[current_box] == None:
 				path.append((detail_points[dest_box], destination_point))
 			current_box = prev[current_box]
-
 	else:
 		print "No path possible!"
 		return ([], [])
@@ -73,8 +113,7 @@ def find_source_box(xy_coord, mesh):
 def find_detail_points(current_coords, end_box):
 
 	x, y = current_coords
-
-
+	
 	e_x1 = end_box[0]
 	e_x2 = end_box[1]
 	e_y1 = end_box[2]
@@ -84,3 +123,14 @@ def find_detail_points(current_coords, end_box):
 	new_y = min(e_y2 - 1, max(e_y1, y))
 
 	return(new_x, new_y)
+
+def heuristic(src, dest):
+	x,y = src
+	x1, y1 = dest
+	#print abs(x - x1) + abs(y - y1)
+	return abs(x - x1) + abs(y - y1)
+
+def cost_path(src, dest):
+	x, y = src
+	x2, y2 = dest
+	return sqrt(x*x2 + y*y2)
